25d2bbe37347591d266153a30840c7c66283e92f
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-11-15 11:04:43 -0600

    modified genesis query - datetime in correct format. add base models.

---
+++ BangazonWorkforce/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+using BangazonWorkforceMVC.Models;
+
+namespace BangazonWorkforceMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Budget { get; set; }
+        public List<Employee> employees { get; set; } = new List<Employee>();
+    }
+}
