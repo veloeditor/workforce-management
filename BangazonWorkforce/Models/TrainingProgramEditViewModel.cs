24885987a1e7d8f5c44e4cf0148e24bb8791f140
Brian <wilsonedit@gmail.com>
2019-11-15 15:06:41 -0600

    TrainingProgram create new works

---
+++ BangazonWorkforce/Models/TrainingProgramEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforceMVC.Models.ViewModel
+{
+    public class TrainingProgramEditViewModel
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MaxAttendees { get; set; }
+    }
+}
