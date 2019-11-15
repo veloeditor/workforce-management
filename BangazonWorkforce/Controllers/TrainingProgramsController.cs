030d4eb641e8d9cbf1ca1d6e112d8f884e329e11
Brian <wilsonedit@gmail.com>
2019-11-15 10:58:02 -0600

    initial commit

---
+++ BangazonWorkforce/Controllers/TrainingProgramsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonWorkforce.Controllers
+{
+    public class TrainingProgramsController : Controller
+    {
+        // GET: TrainingPrograms
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TrainingPrograms/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TrainingPrograms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TrainingPrograms/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainingPrograms/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainingPrograms/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TrainingPrograms/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TrainingPrograms/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BangazonWorkforce/Models/TrainingProgram.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonWorkforce.Models
+{
+    public class TrainingProgram
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string ProgramName { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public int MaxAttendees { get; set; }
+
+        public List<Employee> CurrentAttendees { get; set; } = new List<Employee>();
+
+    }
+}
